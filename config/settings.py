a0bb16a3be1570b7f4fcc0840047bfdde8a3ec6a
Abdumannon Rabbimqulov <abdumannonrab@icloud.com>
2026-04-02 14:54:49 +0500

    fastapi

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# 1. Manzilni kiriting
+# 1. Databse ulanish
 SQLALCHEMY_DATABASE_URL = "postgresql://user@localhost:5432/fastapi"
-# 2. Engine yaratish (bazaga kirish darvozasi)
+# 2. Engine yaratish (bazaga kirish )
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # 3. SessionLocal (har bir so'rov uchun alohida sessiya)
--- users/user.py
+++ users/user.py
@@ -1,14 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends,HTTPException,status
+from jupyterlab.utils import deprecated
+from sqlalchemy.orm import Session
+from config.settings import SessionLocal
+from users import models
+from passlib.context import CryptContext
 
-router=APIRouter(prefix='/users')
+
+pwd_context=CryptContext(schemes=['bcrypt'],deprecated='auto')
+
+
+router=APIRouter(prefix='/users',tags=["Users"])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @router.get("/")
 async def get():
     return {"message":"user bo'limi "}
 
-
-@router.post("/user")
+@router.post("/register")
 async def post():
     pass
 
